b767ad4694eb79b945e7a1bfbb967c5066104047
SamLM11 <samuellm2011@gmail.com>
2023-12-11 15:34:12 -0300

    first commit

---
+++ ImgColorida.py
@@ -0,0 +1,13 @@
+import cv2
+
+img = cv2.imread('poster.jpg')
+
+rocket = img[120:360, 400:500]
+img[0:240, 500:600] = rocket
+
+text = 'Ola mundo'
+cv2.putText(img, text, (20, 220), fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale=0.6, color=(0,0,255) )
+
+cv2.imshow('RocketCard', img)
+
+cv2.waitKey(0)
